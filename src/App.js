36582e04ff90678fd9f9bd3a90a7e724003eebf6
IvanBazhenov4 <93333327+IvanBazhenov4@users.noreply.github.com>
2021-11-12 14:14:28 +0500

    123

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import {Box, Grid, Paper, styled,Button} from "@mui/material";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   const [name, setName] = useState("");
@@ -67,84 +76,117 @@ function App() {
   };
   return (
     <div className="App">
-      <form style={{}}>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 style={{ width: 1260 }}>Регистрация</h1>
-        <h5 style={{ width: 1250 }}>
-          Уже есть аккаунт?
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Войти
-          </a>
-        </h5>
-        <h5 style={{ width: 1200 }}>Имя</h5>
-        {nameHole && nameErroros && (
-          <div style={{ color: "red" }}>{nameErroros}</div>
-        )}
-        <input
-          onChange={(e) => nameHandler(e)}
-          value={name}
-          onBlur={(e) => blurHandler(e)}
-          name="name"
-          type="text"
-          placeholder="Enter your name..."
-        />
-        <h5 style={{ width: 1200, align: "left" }}>Email</h5>
-        {adressHole && adressErroros && (
-          <div style={{ color: "red" }}>{adressErroros}</div>
-        )}
-        <input
-          onChange={(e) => adressHandler(e)}
-          value={adress}
-          onBlur={(e) => blurHandler(e)}
-          name="adress"
-          type="text"
-          placeholder="Enter your adress..."
-        />
-        <h5 style={{ width: 1200 }}>Номер телефона</h5>
-        {numberHole && numberErroros && (
-          <div style={{ color: "red" }}>{numberErroros}</div>
-        )}
-        <input
-          onChange={(e) => numberHandler(e)}
-          value={number}
-          onBlur={(e) => blurHandler(e)}
-          name="number"
-          type="text"
-          placeholder="Enter your number..."
-        />
-        <h5 style={{ width: 1200 }}>Язык</h5>
-        <select defaultValue="Language ">
-          <option value="">Language</option>
-          <option value="value1">Russian</option>
-          <option value="value1">American</option>
-          <option value="value1">Spanish</option>
-          <option value="value1">Chinese</option>
-        </select>
-        <div style={{ display: "flex" }} >
-          <input type="checkbox" />
-          <h5>
-            Accepted
-            <a
-              className="App-link"
-              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              licence
-            </a>
-            user
-          </h5>
-        </div>
-        <button disabled={!formValid} type="submit">
-          Registration
-        </button>
-      </form>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0}>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h2 style={{display: 'flex'}}>Регистрация</h2>
+              <h5 style={{display: 'flex'}}>
+              Уже есть аккаунт?
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                Войти
+              </a>
+            </h5></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h5 style={{display: 'flex'}}>Имя</h5>
+              {nameHole && nameErroros && (
+                  <div style={{ color: "red" }}>{nameErroros}</div>
+              )}
+              <input
+                  style={{display: 'flex',width: 360}}
+                  onChange={(e) => nameHandler(e)}
+                  value={name}
+                  onBlur={(e) => blurHandler(e)}
+                  name="name"
+                  type="text"
+                  placeholder="Введите ваше имя..."
+              /></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h5 style={{display: 'flex'}}>Email</h5>
+              {adressHole && adressErroros && (
+                  <div style={{ color: "red" }}>{adressErroros}</div>
+              )}
+              <input
+                  style={{display: 'flex',width: 360}}
+                  onChange={(e) => adressHandler(e)}
+                  value={adress}
+                  onBlur={(e) => blurHandler(e)}
+                  name="adress"
+                  type="text"
+                  placeholder="Введите ваш почтовый адресс..."
+              /></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h5 style={{display: 'flex'}}>Номер телефона</h5>
+              {numberHole && numberErroros && (
+                  <div style={{ color: "red" }}>{numberErroros}</div>
+              )}
+              <input
+                  style={{display: 'flex',width: 360}}
+                  onChange={(e) => numberHandler(e)}
+                  value={number}
+                  onBlur={(e) => blurHandler(e)}
+                  name="number"
+                  type="text"
+                  placeholder="Введите ваш номер телефона..."
+              /></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h5 style={{display: 'flex'}}>Язык</h5>
+              <select style={{display: 'flex',width: 360}}>
+                <option value="value1">Русский</option>
+                <option value="value1">American</option>
+                <option value="value1">Español</option>
+                <option value="value1">中国人</option>
+              </select></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><h5 style={{display: 'flex'}}><input type="checkbox" />
+
+                Условия
+                <a
+                    className="App-link"
+                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  соглашения
+                </a>
+                пользователя
+              </h5></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Item><Button disabled={!formValid} type="submit">
+              <h4 style={{display: 'flex',width: 360}}>Регистрация</h4>
+            </Button></Item>
+          </Grid>
+          <Grid item xs={4}></Grid>
+
+
+        </Grid>
+      </Box>
     </div>
+
   );
 }
 
